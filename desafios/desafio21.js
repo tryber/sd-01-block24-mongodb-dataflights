986883af5593b86e7271ca25dc8b221d2b384740
antoniopaolillo <antonio_francisco_p@live.com>
2020-03-24 15:48:26 -0300

    desafios 21 e 22

---
+++ desafios/desafio21.js
@@ -0,0 +1,2 @@
+use dataFlights;
+db.voos.find({ litrosCombustivel: { $exists: true}}, { vooId: 1, _id: 0}).limit(1);
---
+++ desafios/desafio22.js
@@ -0,0 +1,2 @@
+use dataFlights;
+db.voos.find({ rtk: { $exists: false}}, { vooId: 1, _id: 0}).limit(1);
