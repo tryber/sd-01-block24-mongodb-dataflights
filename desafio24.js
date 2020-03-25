25c441b55faabcce85bdb918202f49ac0ecfb8eb
henriqueeyer <henriqueeyer@outlook.com>
2020-03-25 15:59:12 -0300

    Finished

---
+++ desafio24.js
@@ -0,0 +1,10 @@
+use dataFlights;
+db.voos.findOne({$or: [
+  { "empresa.nome": "DELTA AIRLINES" },
+  { "empresa.nome": "AMERICAN AIRLINES" }
+],
+$and:[
+  { "aeroportoDestino.sigla":"KJFK" },
+  { "aeroportoOrigem.sigla":"SBGR" }
+]
+},{vooId:1});
